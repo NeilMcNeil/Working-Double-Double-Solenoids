93cb2302f22a2b477811f83547a8036b275d1148
NeilMcNeil <119974057+NeilMcNeil@users.noreply.github.com>
2023-02-24 12:55:48 -0500

    Update DriveSubsystem.java

--- robot/subsystems/DriveSubsystem.java
+++ robot/subsystems/DriveSubsystem.java
@@ -3,6 +3,9 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
+
 
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
@@ -11,13 +14,16 @@ import edu.wpi.first.wpilibj.Joystick;
 import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Robot;
 import frc.robot.RobotContainer;
 import frc.robot.RobotMap;
 import frc.robot.commands.DriveCommand;
+import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
 
 public class DriveSubsystem extends SubsystemBase {
   PWMSparkMax frontRight = new PWMSparkMax(RobotMap.frontRight);
@@ -38,12 +44,18 @@ public class DriveSubsystem extends SubsystemBase {
     DoubleSolenoid exampleDoublePCM2 = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 1, 6);
  
  
-    Compressor pcmCompressor = new Compressor(1, PneumaticsModuleType.CTREPCM);
+    // Compressor pcmCompressor = new Compressor(0, PneumaticsModuleType.CTREPCM);
 
-    
-    boolean enabled = pcmCompressor.isEnabled();
-    boolean pressureSwitch = pcmCompressor.getPressureSwitchValue();
-    double current = pcmCompressor.getCurrent();
+    // boolean enabled = pcmCompressor.isEnabled();
+    // boolean pressureSwitch = pcmCompressor.getPressureSwitchValue();
+    // double current = pcmCompressor.getCurrent();
+
+    // double scale = 250, offset = -25;
+    // AnalogPotentiometer pressureTransducer = new AnalogPotentiometer(/* the AnalogIn port*/ 0, scale, offset);
+
+// scaled values in psi units
+    // double psi = pressureTransducer.get();
+
 
   DifferentialDrive drive;
   /** Creates a new DriverSubsystem. */
@@ -59,26 +71,31 @@ public class DriveSubsystem extends SubsystemBase {
 
     drive = new DifferentialDrive(left, right);
 
-    exampleDoublePCM.set(kOff);
+    // exampleDoublePCM.set(kOff);
     exampleDoublePCM.set(kReverse);
     exampleDoublePCM.set(kForward);
-    pcmCompressor.enableDigital();
-    pcmCompressor.disable();
-    exampleDoublePCM2.set(kOff);
+
+    // pcmCompressor.disable();
+    // exampleDoublePCM2.set(kOff);
     exampleDoublePCM2.set(kReverse);
     exampleDoublePCM2.set(kForward);
+
   }
 
   public void drive(double straight, double rotation){
       drive.arcadeDrive(straight, rotation);
       Joystick joystick = Robot.m_robotContainer.getDriverStick();
+      SmartDashboard.putNumber("Turret Pot Voltage", 8);
       if (joystick.getRawButton(3)) {
         exampleDoublePCM.toggle();
+        // pcmCompressor.enableAnalog(straight, rotation);
+        // pcmCompressor.enableDigital();
             
       }
       if (joystick.getRawButton(4)) {
         exampleDoublePCM2.toggle();
       }
+      
   }
 
   public void stop(){
@@ -90,7 +107,3 @@ public class DriveSubsystem extends SubsystemBase {
     setDefaultCommand(new DriveCommand());
   }
 }
-
-
-
-
